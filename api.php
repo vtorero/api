17b1c147b517fdbc76f9505c3f9a96244b0a2c36
victor jimenez <victor@adpos.pe>
2019-10-29 14:12:36 -0500

    api

--- api.php
+++ api.php
@@ -1,5 +1,6 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Origin:*');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
@@ -7,64 +8,63 @@ $method = $_SERVER['REQUEST_METHOD'];
 if($method == "OPTIONS") {
     die();
 }
-
-
 require_once 'vendor/autoload.php';
-
 $app = new Slim\Slim();
-
 $db = new mysqli("localhost","marife","libido16","adops");
+
 //mysqli_set_charset($db, 'utf8');
 if (mysqli_connect_errno()) {
     printf("Conexión fallida: %s\n", mysqli_connect_error());
     exit();
 }
+$data=array();
 
+$app->get("/skoda",function() use($db,$app){
+    header("Content-type: application/json; charset=utf-8");
+    $resultado = $db->query("SELECT * FROM  adops.skoda");  
+    $clientes=array();
+        while ($fila = $resultado->fetch_array()) {
+            
+            $clientes[]=$fila;
+        }
+        $data = array("status"=>200,"data"=>$clientes);
+        echo  json_encode($data);
+    });
 
 $app->get("/productos",function() use($db,$app){
-
-    $resultado = $db->query("SELECT dimensionad_exchange_device_category,count(*) as total FROM api.11223363888 
-    where dimensionad_exchange_date between '2019-08-01' and '2019-08-25'group by 1 order by 2 desc");
-    
+    header("Content-type: application/json; charset=utf-8");
+    $resultado = $db->query("SELECT dimensionad_exchange_device_category,count(*) as total FROM adops.11223363888 
+    where dimensionad_exchange_date between '2019-09-01' and '2019-09-29' group by 1 order by 2 desc");  
     $productos=array();
         while ($fila = $resultado->fetch_array()) {
             
             $productos[]=$fila;
         }
-
-        echo  json_encode($productos);
+        $data = array("status"=>200,"data"=>$productos);
+        echo  json_encode($data);
     });
-
-
     $app->post("/productos",function() use($db,$app){
-
-        $query ="INSERT INTO productos VALUES (NULL,"
-        ."'{$app->request->post("name")}',"
-        ."'{$app->request->post("description")}',"
-        ."'{$app->request->post("price")}'"
-        .")";
-        $insert= $db->query($query);
-        if($insert){
-            $result = array("STATUS"=>true,"messaje"=>"Producto creado correctamente");
+     $query ="INSERT INTO productos VALUES (NULL,"
+      ."'{$app->request->post("name")}',"
+      ."'{$app->request->post("description")}',"
+      ."'{$app->request->post("price")}'"
+      .")";
+      $insert= $db->query($query);
+       if($insert){
+       $result = array("STATUS"=>true,"messaje"=>"Producto creado correctamente");
         }else{
-            $result = array("STATUS"=>false,"messaje"=>"Producto no creado");
+        $result = array("STATUS"=>false,"messaje"=>"Producto no creado");
         }
-
          echo json_encode($result);
-
-    });        
-    
-    $app->put("/productos/:id",function($id) use($db,$app){
-
+        });        
+        $app->put("/productos/:id",function($id) use($db,$app){
         $query ="UPDATE productos SET "
         ."name ='{$app->request->post("name")}',"
         ."description = '{$app->request->post("description")}',"
         ."price = {$app->request->post("price")} "
         ." WHERE id={$id}";
-
         echo $query;
         $update= $db->query($query);
-
         if($update){
             $result = array("STATUS"=>true,"messaje"=>"Producto actualizado correctamente");
         }else{
@@ -87,9 +87,8 @@ $app->get("/productos",function() use($db,$app){
      echo json_encode($result);
     });
 
-
     $app->post("/skoda",function() use($db,$app){
-        $query ="INSERT INTO skoda (source,origen,nombres,apellidos,rut,telefono,correo,marca,modelo,consesionario)  VALUES ("
+        $query ="INSERT INTO skoda (source,origen,nombres,apellidos,rut,telefono,correo,marca,modelo,concesionario)  VALUES ("
         ."'{$app->request->post("source")}',"
         ."'{$app->request->post("origen")}',"
          ."'{$app->request->post("nombres")}',"
@@ -99,7 +98,7 @@ $app->get("/productos",function() use($db,$app){
          ."'{$app->request->post("correo")}',"
          ."'{$app->request->post("marca")}',"
          ."'{$app->request->post("modelo")}',"
-         ."'{$app->request->post("consesionario")}'"
+         ."'{$app->request->post("concesionario")}'"
          .")";
 
          $insert= $db->query($query);
